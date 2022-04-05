cbcffccaa5a34ee637c354d5887485f92b1b7ee1
caiowf <contato.caioweliton@gmail.com>
2022-04-05 14:55:34 -0300

    feat: :sparkles: handle login and redirects on web application

---
+++ web/src/pages/app/index.tsx
@@ -0,0 +1,18 @@
+import { useUser, withPageAuthRequired } from "@auth0/nextjs-auth0";
+
+export default function Home() {
+  const { user } = useUser();
+
+  return (
+    <div>
+      <h1>Hi, user!</h1>
+      <pre>
+        {JSON.stringify(user, null, 2)}
+      </pre>
+
+      <a href="/api/auth/logout">Logout</a>
+    </div>
+  )
+}
+
+export const getServerSideProps = withPageAuthRequired();
--- web/src/pages/index.tsx
+++ web/src/pages/index.tsx
@@ -1,27 +1,26 @@
-import { getAccessToken, useUser } from "@auth0/nextjs-auth0";
+import { getSession } from "@auth0/nextjs-auth0";
 import { GetServerSideProps } from "next";
 
 export default function Home() {
-  const { user } = useUser()
-  return (
-    <div>
-      <div>Hello World</div>
-
-      <pre>
-        {JSON.stringify(user, null, 2)}
-      </pre>
-
-      <a href="/api/auth/login">Login</a>
-    </div>
-  );
+  return null;
 }
 
-export const getServerSideProps: GetServerSideProps = async ({req, res}) => {
-  const token = getAccessToken(req, res)
+export const getServerSideProps: GetServerSideProps = async ({ req, res }) => {
+  const session = getSession(req, res)
 
-  console.log(token);
+  if (!session) {
+    return {
+      redirect: {
+        destination: '/api/auth/login',
+        permanent: false
+      }
+    }
+  }
 
   return {
-    props: {}
-  } 
+    redirect: {
+      destination: '/app',
+      permanent: false
+    }
+  }
 }
