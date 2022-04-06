c0529bc8b04eedfcffe142afd382eca16c5c50ba
caiowf <contato.caioweliton@gmail.com>
2022-04-06 16:11:50 -0300

    feat: :sparkles: add product creation

--- purchases/src/http/graphql/resolvers/products.resolver.ts
+++ purchases/src/http/graphql/resolvers/products.resolver.ts
@@ -1,7 +1,8 @@
 import { UseGuards } from '@nestjs/common';
-import { Query, Resolver } from '@nestjs/graphql';
+import { Args, Mutation, Query, Resolver } from '@nestjs/graphql';
 import { ProductsService } from '../../../services/product.service';
 import { AuthorizationGuard } from '../../auth/authorization.guard';
+import { CreateProductInput } from '../inputs/create-product-input';
 import { Product } from '../models/product';
 
 @Resolver()
@@ -9,8 +10,13 @@ export class ProductsResolver {
   constructor(private productsService: ProductsService) {}
 
   @Query(() => [Product])
-  // @UseGuards(AuthorizationGuard)
   products() {
     return this.productsService.findAll();
+  }
+
+  @Mutation(() => Product)
+  @UseGuards(AuthorizationGuard)
+  createProduct(@Args('data') data: CreateProductInput) {
+    return this.productsService.create(data);
   }
 }
--- purchases/src/services/product.service.ts
+++ purchases/src/services/product.service.ts
@@ -1,12 +1,28 @@
 import { Injectable } from '@nestjs/common';
 import { PrismaService } from '../database/prisma/prisma.service';
 import { Product } from '../http/graphql/models/product';
-
+import slugify from 'slugify';
+interface CreateProductParams {
+  title: string;
+}
 @Injectable()
 export class ProductsService {
   constructor(private prisma: PrismaService) {}
 
   async findAll(): Promise<Product[]> {
     return this.prisma.product.findMany();
+  }
+
+  async create({ title }: CreateProductParams): Promise<Product> {
+    const slug = slugify(title, { lower: true });
+
+    const productWithSameSlug = await this.prisma.product.findUnique({
+      where: { slug },
+    });
+
+    if (productWithSameSlug)
+      throw new Error('Another product with same slug already exists');
+
+    return this.prisma.product.create({ data: { title, slug } });
   }
 }
