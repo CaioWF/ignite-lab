a21b829369123b992744118276d1210603ef93f8
caiowf <contato.caioweliton@gmail.com>
2022-04-30 16:08:12 -0300

    refactor: :recycle: change port

--- purchases/src/main.ts
+++ purchases/src/main.ts
@@ -1,7 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
-const PORT = 3333;
+const PORT = 3334;
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   await app.listen(PORT, () => console.log(`Server running at ${PORT}`));
